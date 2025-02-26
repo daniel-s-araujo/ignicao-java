fff86cb7b863f066c77c3dfe2500cadac1f331b5
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-02-25 21:33:21 -0300

    2.1 Herança e sobrescrita de métodos

--- banco/src/com/algaworks/banco/app/Principal.java
+++ banco/src/com/algaworks/banco/app/Principal.java
@@ -1,6 +1,8 @@
 package com.algaworks.banco.app;
 
 import com.algaworks.banco.modelo.Conta;
+import com.algaworks.banco.modelo.ContaEspecial;
+import com.algaworks.banco.modelo.ContaInvestimento;
 import com.algaworks.banco.modelo.Pessoa;
 
 public class Principal {
@@ -14,16 +16,18 @@ public class Principal {
         titular2.setNome("Maria Abadia");
         titular2.setDocumento("22233344455");
 
-        Conta minhaConta = new Conta(titular1, 123, 987);
-        Conta suaConta = new Conta(titular2, 222, 333);
+        ContaInvestimento minhaConta = new ContaInvestimento(titular1, 123, 987);
+        ContaEspecial suaConta = new ContaEspecial(titular2, 222, 333, 1_000);
 
-        minhaConta.depositar(15_000);
-        minhaConta.sacar(1_000, 10);
+//        minhaConta.depositar(15_000);
+//        minhaConta.sacar(1_000);
+//        minhaConta.creditarRendimentos(0.8);
 
-        suaConta.depositar(30_000);
+        suaConta.depositar(15_000);
+        suaConta.sacar(15_500);
 
-        System.out.println("Titular: " + minhaConta.getTitular().getNome());
-        System.out.println("Saldo: " + minhaConta.getSaldo());
+//        System.out.println("Titular: " + minhaConta.getTitular().getNome());
+//        System.out.println("Saldo: " + minhaConta.getSaldo());
 
         System.out.println("Titular: " + suaConta.getTitular().getNome());
         System.out.println("Saldo: " + suaConta.getSaldo());
