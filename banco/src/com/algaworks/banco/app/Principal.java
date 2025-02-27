8f47ceaddced50c7d49039aa331ca0d868d55c31
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-02-26 21:36:09 -0300

    2.2 Upcasting e polimorfismo

--- banco/src/com/algaworks/banco/app/Principal.java
+++ banco/src/com/algaworks/banco/app/Principal.java
@@ -1,9 +1,6 @@
 package com.algaworks.banco.app;
 
-import com.algaworks.banco.modelo.Conta;
-import com.algaworks.banco.modelo.ContaEspecial;
-import com.algaworks.banco.modelo.ContaInvestimento;
-import com.algaworks.banco.modelo.Pessoa;
+import com.algaworks.banco.modelo.*;
 
 public class Principal {
 
@@ -16,21 +13,21 @@ public class Principal {
         titular2.setNome("Maria Abadia");
         titular2.setDocumento("22233344455");
 
+        CaixaEletronico caixaEletronico = new CaixaEletronico();
+
         ContaInvestimento minhaConta = new ContaInvestimento(titular1, 123, 987);
         ContaEspecial suaConta = new ContaEspecial(titular2, 222, 333, 1_000);
 
-//        minhaConta.depositar(15_000);
-//        minhaConta.sacar(1_000);
-//        minhaConta.creditarRendimentos(0.8);
+        minhaConta.depositar(15_000);
+        minhaConta.sacar(1_000);
+        minhaConta.creditarRendimentos(0.8);
 
         suaConta.depositar(15_000);
         suaConta.sacar(15_500);
 
-//        System.out.println("Titular: " + minhaConta.getTitular().getNome());
-//        System.out.println("Saldo: " + minhaConta.getSaldo());
-
-        System.out.println("Titular: " + suaConta.getTitular().getNome());
-        System.out.println("Saldo: " + suaConta.getSaldo());
+        caixaEletronico.imprimirSaldo(minhaConta);
+        System.out.println();
+        caixaEletronico.imprimirSaldo(suaConta);
     }
 
 }
