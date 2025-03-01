fecfe076fd735d73ef318526a37c1fbbcc010578
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-03-01 12:57:33 -0300

    2.5 Exceções

--- banco/src/com/algaworks/banco/app/Principal.java
+++ banco/src/com/algaworks/banco/app/Principal.java
@@ -2,8 +2,8 @@ package com.algaworks.banco.app;
 
 import com.algaworks.banco.modelo.*;
 import com.algaworks.banco.modelo.atm.CaixaEletronico;
+import com.algaworks.banco.modelo.excecao.SaldoInsuficienteException;
 import com.algaworks.banco.modelo.pagamento.Boleto;
-import com.algaworks.banco.modelo.pagamento.DocumentoPagavel;
 import com.algaworks.banco.modelo.pagamento.Holerite;
 
 public class Principal {
@@ -22,28 +22,27 @@ public class Principal {
         ContaInvestimento minhaConta = new ContaInvestimento(titular1, 123, 987);
         ContaEspecial suaConta = new ContaEspecial(titular2, 222, 333, 1_000);
 
-        minhaConta.depositar(30_000);
-        minhaConta.sacar(1_000);
-//        minhaConta.creditarRendimentos(0.8);
-//        minhaConta.debitarTarifaMensal();
+        try {
+            minhaConta.depositar(30_000);
+            minhaConta.sacar(1_000);
 
-        suaConta.depositar(15_000);
-        suaConta.sacar(15_500);
-        suaConta.debitarTarifaMensal();
+            suaConta.depositar(15_000);
+            suaConta.sacar(15_500);
+            suaConta.debitarTarifaMensal();
 
-        Boleto boletoEscola = new Boleto(titular2, 800);
-        Holerite salarioFuncionario = new Holerite(titular2, 100, 160);
+            Boleto boletoEscola = new Boleto(titular2, 35_000);
+            Holerite salarioFuncionario = new Holerite(titular2, 100, 160);
 
-        caixaEletronico.pagar(boletoEscola, minhaConta);
-        caixaEletronico.pagar(salarioFuncionario, minhaConta);
+            caixaEletronico.pagar(boletoEscola, minhaConta);
+            caixaEletronico.pagar(salarioFuncionario, minhaConta);
 
-        caixaEletronico.estornarPagamento(boletoEscola, minhaConta);
+            caixaEletronico.estornarPagamento(boletoEscola, minhaConta);
 
-        boletoEscola.imprimirRecibo();
-        salarioFuncionario.imprimirRecibo();
-
-//        System.out.println("Boleto pago: " + boletoEscola.estaPago());
-//        System.out.println("Salário pago: " + salarioFuncionario.estaPago());
+            boletoEscola.imprimirRecibo();
+            salarioFuncionario.imprimirRecibo();
+        } catch (SaldoInsuficienteException e) {
+            System.out.println("Erro ao executar operação na conta: " + e.getMessage());
+        }
 
         caixaEletronico.imprimirSaldo(minhaConta);
         System.out.println();
