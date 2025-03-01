d26fdf8889dc1477d2f1a213c3df80b3c8a9b7c2
Daniel Araújo <daniels_araujo@outlook.com.br>
2025-02-28 22:11:49 -0300

    2.4 Interfaces

--- banco/src/com/algaworks/banco/app/Principal.java
+++ banco/src/com/algaworks/banco/app/Principal.java
@@ -1,6 +1,10 @@
 package com.algaworks.banco.app;
 
 import com.algaworks.banco.modelo.*;
+import com.algaworks.banco.modelo.atm.CaixaEletronico;
+import com.algaworks.banco.modelo.pagamento.Boleto;
+import com.algaworks.banco.modelo.pagamento.DocumentoPagavel;
+import com.algaworks.banco.modelo.pagamento.Holerite;
 
 public class Principal {
 
@@ -18,14 +22,28 @@ public class Principal {
         ContaInvestimento minhaConta = new ContaInvestimento(titular1, 123, 987);
         ContaEspecial suaConta = new ContaEspecial(titular2, 222, 333, 1_000);
 
-        minhaConta.depositar(15_000);
+        minhaConta.depositar(30_000);
         minhaConta.sacar(1_000);
-        minhaConta.creditarRendimentos(0.8);
-        minhaConta.debitarTarifaMensal();
+//        minhaConta.creditarRendimentos(0.8);
+//        minhaConta.debitarTarifaMensal();
 
         suaConta.depositar(15_000);
         suaConta.sacar(15_500);
         suaConta.debitarTarifaMensal();
+
+        Boleto boletoEscola = new Boleto(titular2, 800);
+        Holerite salarioFuncionario = new Holerite(titular2, 100, 160);
+
+        caixaEletronico.pagar(boletoEscola, minhaConta);
+        caixaEletronico.pagar(salarioFuncionario, minhaConta);
+
+        caixaEletronico.estornarPagamento(boletoEscola, minhaConta);
+
+        boletoEscola.imprimirRecibo();
+        salarioFuncionario.imprimirRecibo();
+
+//        System.out.println("Boleto pago: " + boletoEscola.estaPago());
+//        System.out.println("Salário pago: " + salarioFuncionario.estaPago());
 
         caixaEletronico.imprimirSaldo(minhaConta);
         System.out.println();
